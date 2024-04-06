99390a5ab39ebe0cdb4fbd606a1f4b4580dc9059
anand <anandanand99599@gmail.com>
2024-04-06 20:38:10 +0530

    expense project setup

--- variable.tf
+++ variable.tf
@@ -7,10 +7,10 @@ variable "tools" {
       grafana = {
         instance_type        = "t3.micro"
         policy_resource_list = ["ec2:DescribeInstance"]
+      }
     }
   }
 }
-
 variable "zone_id"  {
   default = "Z0738462OKKUK2E41LUJ"
 }
