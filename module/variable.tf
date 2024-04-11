d453e35ca51d7cf226378d3014470519c097c977
anand <anandanand99599@gmail.com>
2024-04-11 17:14:29 +0530

    adding dummy policy

--- module/variable.tf
+++ module/variable.tf
@@ -3,5 +3,5 @@ variable "instance_type" {}
 variable "zone_id" {}
 variable "policy_resource_list" {}
 variable "dummy_policy" {
-  Default: ["ec2=DescribeInstanceTypes"]
+  Default = ["ec2=DescribeInstanceTypes"]
 }
