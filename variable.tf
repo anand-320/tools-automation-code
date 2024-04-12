e7dc04965d4cf940a9808fdee74478056978e35e
anand <anandanand99599@gmail.com>
2024-04-12 11:52:13 +0530

    adding vault

--- variable.tf
+++ variable.tf
@@ -10,10 +10,10 @@ variable "tools" {
       policy_resource_list = ["ec2:DescribeInstances"]
     }
 
-    vault = {
-      instance_type        = "t3.small"
-      policy_resource_list = []
-    }
+  #  vault = {
+   #   instance_type        = "t3.small"
+   #   policy_resource_list = []
+   # }
   }
 }
 variable "zone_id"  {
