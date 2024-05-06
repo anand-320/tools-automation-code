2bcd8a77843c1c669763c21541d109f9c1380817
anand <anandanand99599@gmail.com>
2024-05-06 21:45:18 +0530

    Adding developer code

--- variable.tf
+++ variable.tf
@@ -24,6 +24,11 @@ variable "tools" {
       instance_type        = "t3.small"
       policy_resource_list = []
     }
+
+    sonarqube = {
+      instance_type        = "t3.large"
+      policy_resource_list = []
+    }
   }
 }
 variable "zone_id"  {
